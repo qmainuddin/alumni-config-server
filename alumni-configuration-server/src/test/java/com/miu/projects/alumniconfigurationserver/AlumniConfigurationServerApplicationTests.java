8805da6832b67246d779d1ad1561d44f0a6d6e57
Mainuddin Talukdar <mtalukdar@vantage.com>
2023-11-21 01:25:57 -0600

    completed config server for auth-service

---
+++ alumni-configuration-server/src/test/java/com/miu/projects/alumniconfigurationserver/AlumniConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.projects.alumniconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
